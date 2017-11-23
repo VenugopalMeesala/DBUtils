bb40a3766b53c6b43b401e8f1d8bafd60cac88a7
venugopal meesala <venugopal_meesala@gap.com>
2017-11-22 23:55:11 -0800

    [Venugopal] home page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SelectDbutilsComponent } from './select-dbutils/select-dbutils.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectDbutilsComponent
+    SelectDbutilsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
