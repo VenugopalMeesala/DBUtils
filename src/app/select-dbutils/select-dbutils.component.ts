5353aada933428a8d61beae762246b38aa64c1aa
venugopal meesala <venugopal_meesala@gap.com>
2017-11-22 22:09:42 -0800

    [Venugopal] basic structure

---
+++ src/app/select-dbutils/select-dbutils.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-dbutils',
+  templateUrl: './select-dbutils.component.html',
+  styleUrls: ['./select-dbutils.component.css']
+})
+export class SelectDbutilsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
